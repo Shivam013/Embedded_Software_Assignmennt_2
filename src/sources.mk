c413f4c2675257ddf575f70ca0c0bae6e32580e6
Shivam Amin <shivam@Mac.attlocal.net>
2024-09-21 12:52:28 -0700

    Completed functionality

--- src/sources.mk
+++ src/sources.mk
@@ -9,9 +9,18 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES =
+ifeq ($(PLATFORM),HOST)
+    SOURCES = main.c memory.c
+    INCLUDES = -I../include/common
 
-# Add your include paths to this variable
-INCLUDES = 
+else ifeq ($(PLATFORM),MSP432)
+    SOURCES = main.c memory.c \
+              interrupts_msp432p401r_gcc.c \
+              startup_msp432p401r_gcc.c \
+              system_msp432p401r.c
 
+    INCLUDES = -I../include/common \
+               -I../include/msp432 \
+               -I../include/CMSIS
+
+endif
